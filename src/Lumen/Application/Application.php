9e72ba595f8e49ae4cc5b7be6d04a26dd98c367d
ChenL <chenlinxh@gmail.com>
2017-10-07 16:29:51 +0800

    set dispatcher after init

--- src/Lumen/Application/Application.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace LumenPress\Lumen;
-
-class Application extends \Laravel\Lumen\Application
-{
-    use DispatcherResolver;
-}
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -14,14 +14,8 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
         });
 
         if ($this->isLumen()) {
-            $this->app->setDispatcherResolver(function () {
-                return \FastRoute\simpleDispatcher(function ($r) {
-                    $routes = property_exists($this->app, 'router') ? $this->app->router->getRoutes() : $this->app->getRoutes();
-
-                    foreach ($routes as $route) {
-                        $r->addRoute($route['method'], $route['uri'], $route['action']);
-                    }
-                }, ['dispatcher' => GroupCountBasedDispatcher::class]);
+            add_action('init', function () {
+                $this->app->setDispatcher($this->createDispatcher());
             });
         } else {
             $this->app->singleton('router', function ($app) {
@@ -33,5 +27,18 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
     public function isLumen($version = null)
     {
         return stripos($this->app->version(), 'Lumen') !== false;
+    }
+
+    protected function createDispatcher()
+    {
+        return \FastRoute\simpleDispatcher(function ($r) {
+            $routes = property_exists($this->app, 'router') 
+                ? $this->app->router->getRoutes() 
+                : $this->app->getRoutes();
+
+            foreach ($routes as $route) {
+                $r->addRoute($route['method'], $route['uri'], $route['action']);
+            }
+        }, ['dispatcher' => GroupCountBasedDispatcher::class]);
     }
 }
